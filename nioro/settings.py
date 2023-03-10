c17cc5959ae2aea4af57c9ca5fa8ad1c4cbdfa2a
WailKouicemm <101293943+WailKouicemm@users.noreply.github.com>
2023-03-10 20:39:35 +0100

    Update settings.py

--- nioro/settings.py
+++ nioro/settings.py
@@ -81,15 +81,10 @@ WSGI_APPLICATION = 'nioro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'pcs_db',
-        'USER':'root',
-        'PASSWORD':'28131320Wx.',
-        'HOST':'localhost',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
